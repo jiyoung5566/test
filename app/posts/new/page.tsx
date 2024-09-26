7dd50ea63852bf4afc6ed02876ddf79439dd7b38
unknown <mbmtms@nver.com>
2024-09-26 17:08:20 +0900

    1st

---
+++ app/posts/new/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NewPostPage() {
+  return (
+    <div>
+      <h1>New Post Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo natus,
+        necessitatibus, modi fuga soluta culpa saepe amet omnis, ratione quos
+        labore at rem? Molestiae error beatae recusandae aliquid provident quae!
+      </p>
+    </div>
+  )
+}
